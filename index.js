c48761456cb6fab7b8a88f7383bacedf9fa1c1da
Cameron Bytheway <cameron@nujii.com>
2013-02-11 11:42:05 -0700

    Expose the a hook in middleware

--- index.js
+++ index.js
@@ -7,24 +7,30 @@
  *
  * @param {Array} vars
  * @param {Object} options
+ * @param {Function} hook
  * @return {Function}
  */
-module.exports = function(vars, options) {
+module.exports = function(vars, options, hook) {
   vars = vars || (process.env.BROWSER_ENV || "").split(",");
   options = options || {maxage: 3600000};
-
   options.name = options.name || "browser-env";
+  hook = hook || function(req, vars, next) {next()};
 
   return function browserEnv(req, res, next) {
     var browser = {};
     vars.forEach(function(env) {
       browser[env] = process.env[env];
     });
-    var cookie = JSON.stringify(browser);
 
-    if (req.cookies && req.cookies[options.name] === cookie) return next();
+    hook(req, browser, function(err, browserVars) {
+      if(err) return next(err);
 
-    res.cookie(options.name, cookie, options);
-    next();
+      var cookie = JSON.stringify(browserVars || browser);
+
+      if (req.cookies && req.cookies[options.name] === cookie) return next();
+
+      res.cookie(options.name, cookie, options);
+      next();
+    });
   };
 };
